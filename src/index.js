f50a445671d154d1e891f18a083ecf2bf0221d55
Mark <Trrrrex93@gmail.com>
2020-04-06 20:25:39 +0400

    add subscribe

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import {rerender} from './rerender'
 import state, { startTimer } from './state/state'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {subscribe} from './state/state'
 
+let rerender = () =>{
+    ReactDOM.render(
+        <App state={state}/>,
+      document.getElementById('root')
+   );
+}
 startTimer()
 rerender(state)
+subscribe(rerender)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/state/state.js
+++ src/state/state.js
@@ -1,29 +1,30 @@
-import {rerender} from './../rerender'
+let rerender = () => {};
 let state = {
-  orderInfo:[
-    {id: 1, price: 120,num: "1442_164", company: `SO_GURMAN` }
-  ],
-  countDown: {m:20, s:0 }
+  orderInfo: [{ id: 1, price: 120, num: "1442_164", company: `SO_GURMAN` }],
+  countDown: { m: 20, s: 0 },
 };
 
-let m = state.countDown.m;
-let s = state.countDown.s;
-export let startTimer =() => {
-    if (s == 0) {
-      if (m == 0) {
-        console.log("Время вышло");
-        return;
-      }
-      m--;
-      if (m < 10) m = "0" + m;
-      s = 59;
+export let startTimer = () => {
+  let m = state.countDown.m;
+  let s = state.countDown.s;
+  if (s == 0) {
+    if (m == 0) {
+      console.log("Время вышло");
+      return;
     }
-    else s--;
-    if (s < 10) s = "0" + s;
-    state.countDown.m = m;
-    state.countDown.s = s;
-    setTimeout(startTimer, 1000);
-    rerender(state)
+    m--;
+    if (m < 10) m = "0" + m;
+    s = 59;
+  } else s--;
+  if (s < 10) s = "0" + s;
+  state.countDown.m = m;
+  state.countDown.s = s;
+  setTimeout(startTimer, 1000);
+  rerender();
+};
+
+export let subscribe = (observe) =>{
+  rerender = observe
 }
 
-export default state
+export default state;
