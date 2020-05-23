15f5a698c9f2f44760f6e3e75f7da0bf0fb03995
Mark <Trrrrex93@gmail.com>
2020-05-23 18:57:20 +0400

    Updates

---
+++ precache-manifest.6cde9372c65fc74faf321e2835a1bfe6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5506a12c9074af5a6aebd0a2072b09d",
+    "url": "/pay-sys/index.html"
+  },
+  {
+    "revision": "e5cf90aaf845835d4d1d",
+    "url": "/pay-sys/static/css/main.1bfa1a39.chunk.css"
+  },
+  {
+    "revision": "c4e75cc2d04c1873303d",
+    "url": "/pay-sys/static/js/2.c4f9fe5f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pay-sys/static/js/2.c4f9fe5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5cf90aaf845835d4d1d",
+    "url": "/pay-sys/static/js/main.03a42001.chunk.js"
+  },
+  {
+    "revision": "fd6e9838c9899a83c10f",
+    "url": "/pay-sys/static/js/runtime-main.5de5b057.js"
+  }
+]);
